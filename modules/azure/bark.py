3cd2179f4372b2283bf0994d4c2075958c6ccf6f
Justin Baker <64592089+jus0xA@users.noreply.github.com>
2023-04-11 18:36:08 -0500

    Update bark.py

--- modules/azure/bark.py
+++ modules/azure/bark.py
@@ -1,1 +1,29 @@
+#!/usr/bin/env python
+#####################################
+# Installation module for BARK
+#####################################
 
+# AUTHOR OF MODULE NAME
+AUTHOR="Justin Baker (jus0xA)"
+
+# DESCRIPTION OF THE MODULE
+DESCRIPTION="This module will install/update BARK - a PowerShell script to assist with researching and validating AAD abuse primitives."
+
+# INSTALL TYPE GIT, SVN, FILE DOWNLOAD
+# OPTIONS = GIT, SVN, FILE
+INSTALL_TYPE="GIT"
+
+# LOCATION OF THE FILE OR GIT/SVN REPOSITORY
+REPOSITORY_LOCATION="https://github.com/BloodHoundAD/BARK"
+
+# WHERE DO YOU WANT TO INSTALL IT
+INSTALL_LOCATION="BARK"
+
+# DEPENDS FOR DEBIAN INSTALLS
+DEBIAN="git"
+
+# DEPENDS FOR FEDORA INSTALLS
+FEDORA="git"
+
+# COMMANDS TO RUN AFTER
+AFTER_COMMANDS=""
