55b3c39bc991debede7781950c41659d265cc586
Justin Baker <64592089+jus0xA@users.noreply.github.com>
2023-04-11 18:31:32 -0500

    Update powerzure.py

--- modules/azure/powerzure.py
+++ modules/azure/powerzure.py
@@ -1,1 +1,29 @@
+#!/usr/bin/env python
+#####################################
+# Installation module for PowerZure
+#####################################
 
+# AUTHOR OF MODULE NAME
+AUTHOR="Justin Baker (jus0xA)"
+
+# DESCRIPTION OF THE MODULE
+DESCRIPTION="This module will install/update PowerZure - a project to assess and exploit resources within Microsoft Azure"
+
+# INSTALL TYPE GIT, SVN, FILE DOWNLOAD
+# OPTIONS = GIT, SVN, FILE
+INSTALL_TYPE="GIT"
+
+# LOCATION OF THE FILE OR GIT/SVN REPOSITORY
+REPOSITORY_LOCATION="https://github.com/hausec/PowerZure"
+
+# WHERE DO YOU WANT TO INSTALL IT
+INSTALL_LOCATION="PowerZure"
+
+# DEPENDS FOR DEBIAN INSTALLS
+DEBIAN="git"
+
+# DEPENDS FOR FEDORA INSTALLS
+FEDORA="git"
+
+# COMMANDS TO RUN AFTER
+AFTER_COMMANDS=""
