8db7fb99d69d8ed0d120696e2b2b90db6f0ab77c
Justin Baker <64592089+jus0xA@users.noreply.github.com>
2023-04-11 18:07:50 -0500

    Create tokenman.py

---
+++ modules/azure/tokenman.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+#####################################
+# Installation module for TokenMan
+#####################################
+
+# AUTHOR OF MODULE NAME
+AUTHOR="Justin Baker (jus0xA)"
+
+# DESCRIPTION OF THE MODULE
+DESCRIPTION="This module will install/update TokenMan - and access and refresh token suite for AAD"
+
+# INSTALL TYPE GIT, SVN, FILE DOWNLOAD
+# OPTIONS = GIT, SVN, FILE
+INSTALL_TYPE="GIT"
+
+# LOCATION OF THE FILE OR GIT/SVN REPOSITORY
+REPOSITORY_LOCATION="https://github.com/secureworks/TokenMan"
+
+# WHERE DO YOU WANT TO INSTALL IT
+INSTALL_LOCATION="TokenMan"
+
+# DEPENDS FOR DEBIAN INSTALLS
+DEBIAN="git"
+
+# DEPENDS FOR FEDORA INSTALLS
+FEDORA="git"
+
+# COMMANDS TO RUN AFTER
+AFTER_COMMANDS=""
