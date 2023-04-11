aac2f356266e8567ff4687e4d246a750c8e4189f
Justin Baker <64592089+jus0xA@users.noreply.github.com>
2023-04-11 18:33:38 -0500

    Update microburst.py

--- modules/azure/microburst.py
+++ modules/azure/microburst.py
@@ -1,1 +1,29 @@
+#!/usr/bin/env python
+#####################################
+# Installation module for MicroBurst
+#####################################
 
+# AUTHOR OF MODULE NAME
+AUTHOR="Justin Baker (jus0xA)"
+
+# DESCRIPTION OF THE MODULE
+DESCRIPTION="This module will install/update MicroBurst - a PowerShell toolkit for attacking Microsoft Azure"
+
+# INSTALL TYPE GIT, SVN, FILE DOWNLOAD
+# OPTIONS = GIT, SVN, FILE
+INSTALL_TYPE="GIT"
+
+# LOCATION OF THE FILE OR GIT/SVN REPOSITORY
+REPOSITORY_LOCATION="https://github.com/NetSPI/MicroBurst"
+
+# WHERE DO YOU WANT TO INSTALL IT
+INSTALL_LOCATION="MicroBurst"
+
+# DEPENDS FOR DEBIAN INSTALLS
+DEBIAN="git"
+
+# DEPENDS FOR FEDORA INSTALLS
+FEDORA="git"
+
+# COMMANDS TO RUN AFTER
+AFTER_COMMANDS=""
